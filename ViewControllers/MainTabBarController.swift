21f3ca9b1eb390845550279f702b391d000c4aa5
Unknown <mohamad.farhoudi@hotmail.co.uk>
2018-03-08 14:10:23 +0000

    Adds plusViewController + Deactivates navBaritem

--- ViewControllers/MainTabBarController.swift
+++ ViewControllers/MainTabBarController.swift
@@ -11,12 +11,27 @@ import UIKit
 import Firebase
 
 
-class MainTabBarController: UITabBarController {
+class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        let index = viewControllers?.index(of: viewController)
+        
+        if index == 2 {
+            let plusViewController = PlusViewController()
+            let navController = UINavigationController(rootViewController: plusViewController)
+            present(navController, animated: true, completion: nil)
+            return false
+        }
+        return true
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.delegate = self
+    
         if Auth.auth().currentUser == nil {
             DispatchQueue.main.async {
                 let LogInController = LogInViewController()
---
+++ ViewControllers/PlusViewController.swift
@@ -0,0 +1,20 @@
+//
+//  plusViewController.swift
+//  InstagramSampleApp
+//
+//  Created by Mohammad Farhoudi on 08/03/2018.
+//  Copyright © 2018 Mohammad Farhoudi. All rights reserved.
+//
+
+import UIKit
+
+
+
+class PlusViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+    }
+}
